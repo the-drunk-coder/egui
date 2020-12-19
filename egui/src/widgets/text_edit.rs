7acedc4c763e3d854c19b03a7580d12572bb331d
nik <nik@parkellipsen.de>
2020-12-20 00:24:19 +0100

    call-backable textbox

--- egui/src/widgets/text_edit.rs
+++ egui/src/widgets/text_edit.rs
@@ -375,10 +375,381 @@ impl<'t> Widget for TextEdit<'t> {
                     Event::Key {
                         key: Key::Enter,
                         pressed: true,
+                        ..
+                    } => {
+			if multiline {
+                            let mut ccursor = delete_selected(text, &cursorp);
+                            insert_text(&mut ccursor, text, "\n");
+                            Some(CCursorPair::one(ccursor))
+                        } else {
+                            // Common to end input with enter
+                            ui.memory().surrender_kb_focus(id);
+                            break;
+                        }
+                    }
+                    Event::Key {
+                        key: Key::Escape,
+                        pressed: true,
+                        ..
+                    } => {
+                        ui.memory().surrender_kb_focus(id);
+                        break;
+                    }
+
+                    Event::Key {
+                        key: Key::Z,
+                        pressed: true,
+                        modifiers,
+                    } if modifiers.command && !modifiers.shift => {
+                        // TODO: redo
+                        if let Some((undo_ccursorp, undo_txt)) =
+                            state.undoer.undo(&(cursorp.as_ccursorp(), text.clone()))
+                        {
+                            *text = undo_txt.clone();
+                            Some(*undo_ccursorp)
+                        } else {
+                            None
+                        }
+                    }
+
+                    Event::Key {
+                        key,
+                        pressed: true,
+                        modifiers,
+                    } => on_key_press(&mut cursorp, text, &galley, *key, modifiers),
+
+                    Event::Key { .. } => None,
+                };
+
+                if let Some(new_ccursorp) = did_mutate_text {
+                    // Layout again to avoid frame delay, and to keep `text` and `galley` in sync.
+                    let font = &ui.fonts()[text_style];
+                    galley = if multiline {
+                        font.layout_multiline(text.clone(), available_width)
+                    } else {
+                        font.layout_single_line(text.clone())
+                    };
+
+                    // Set cursorp using new galley:
+                    cursorp = CursorPair {
+                        primary: galley.from_ccursor(new_ccursorp.primary),
+                        secondary: galley.from_ccursor(new_ccursorp.secondary),
+                    };
+                }
+            }
+            state.cursorp = Some(cursorp);
+
+            state
+                .undoer
+                .feed_state(ui.input().time, &(cursorp.as_ccursorp(), text.clone()));
+        }
+
+        {
+            let visuals = ui.style().interact(&response);
+            let bg_rect = response.rect.expand(2.0); // breathing room for content
+            ui.painter().add(PaintCmd::Rect {
+                rect: bg_rect,
+                corner_radius: visuals.corner_radius,
+                fill: ui.style().visuals.dark_bg_color,
+                // fill: visuals.bg_fill,
+                stroke: visuals.bg_stroke,
+            });
+        }
+
+        if ui.memory().has_kb_focus(id) {
+            if let Some(cursorp) = state.cursorp {
+                paint_cursor_selection(ui, response.rect.min, &galley, &cursorp);
+                paint_cursor_end(ui, response.rect.min, &galley, &cursorp.primary);
+            }
+        }
+
+        let text_color = text_color
+            .or(ui.style().visuals.override_text_color)
+            // .unwrap_or_else(|| ui.style().interact(&response).text_color()); // too bright
+            .unwrap_or_else(|| ui.style().visuals.widgets.inactive.text_color());
+        ui.painter()
+            .galley(response.rect.min, galley, text_style, text_color);
+
+        ui.memory().text_edit.insert(id, state);
+
+        Response {
+            lost_kb_focus: ui.memory().lost_kb_focus(id), // we may have lost it during the course of this function
+            ..response
+        }
+    }
+}
+
+/// A text region that the user can edit the contents of, and call a
+/// a callback on the selection on Ctrl+Enter
+///
+/// Example:
+///
+/// ```
+/// # let mut ui = egui::Ui::__test();
+/// # let mut my_string = String::new();
+/// let response = ui.add(egui::TextEdit::singleline(&mut my_string));
+/// if response.lost_kb_focus {
+///     // use my_string
+/// }
+/// ```
+use std::sync::*;
+use parking_lot::Mutex;
+
+pub struct CallbackTextEdit<'t>  {
+    text: &'t mut String,
+    id: Option<Id>,
+    id_source: Option<Id>,
+    text_style: Option<TextStyle>,
+    text_color: Option<Srgba>,
+    multiline: bool,
+    enabled: bool,
+    desired_width: Option<f32>,
+    desired_height_rows: usize,
+    eval_callback: Option<Arc<Mutex<dyn FnMut(&String)>>>,
+}
+
+impl<'t> CallbackTextEdit<'t>  {
+            
+    /// A `TextEdit` for multiple lines. Pressing enter key will create a new line.
+    pub fn multiline(text: &'t mut String) -> Self {
+        CallbackTextEdit {
+            text,
+            id: None,
+            id_source: None,
+            text_style: None,
+            text_color: None,
+            multiline: true,
+            enabled: true,
+            desired_width: None,
+            desired_height_rows: 4,
+	    eval_callback: None,
+        }
+    }
+
+    pub fn id(mut self, id: Id) -> Self {
+        self.id = Some(id);
+        self
+    }
+
+    pub fn eval_callback(mut self, callback: &Arc<Mutex<dyn FnMut(&String)>>) -> Self {
+        self.eval_callback = Some(Arc::clone(callback));
+        self
+    }
+
+    /// A source for the unique `Id`, e.g. `.id_source("second_text_edit_field")` or `.id_source(loop_index)`.
+    pub fn id_source(mut self, id_source: impl std::hash::Hash) -> Self {
+        self.id_source = Some(Id::new(id_source));
+        self
+    }
+
+    pub fn text_style(mut self, text_style: TextStyle) -> Self {
+        self.text_style = Some(text_style);
+        self
+    }
+
+    pub fn text_color(mut self, text_color: Srgba) -> Self {
+        self.text_color = Some(text_color);
+        self
+    }
+
+    pub fn text_color_opt(mut self, text_color: Option<Srgba>) -> Self {
+        self.text_color = text_color;
+        self
+    }
+
+    /// Default is `true`. If set to `false` then you cannot edit the text.
+    pub fn enabled(mut self, enabled: bool) -> Self {
+        self.enabled = enabled;
+        self
+    }
+
+    /// Set to 0.0 to keep as small as possible
+    pub fn desired_width(mut self, desired_width: f32) -> Self {
+        self.desired_width = Some(desired_width);
+        self
+    }
+
+    /// Set the number of rows to show by default.
+    /// The default for singleline text is `1`.
+    /// The default for multiline text is `4`.
+    pub fn desired_rows(mut self, desired_height_rows: usize) -> Self {
+        self.desired_height_rows = desired_height_rows;
+        self
+    }
+}
+
+impl<'t> Widget for CallbackTextEdit<'t>  {
+    fn ui(self, ui: &mut Ui) -> Response {
+        let CallbackTextEdit {
+            text,
+            id,
+            id_source,
+            text_style,
+            text_color,
+            multiline,
+            enabled,
+            desired_width,
+            desired_height_rows,
+	    eval_callback,
+        } = self;
+
+        let text_style = text_style.unwrap_or_else(|| ui.style().body_text_style);
+        let font = &ui.fonts()[text_style];
+        let line_spacing = font.row_height();
+        let available_width = ui.available_width();
+        let mut galley = if multiline {
+            font.layout_multiline(text.clone(), available_width)
+        } else {
+            font.layout_single_line(text.clone())
+        };
+
+        let desired_width = desired_width.unwrap_or_else(|| ui.style().spacing.text_edit_width);
+        let desired_height = (desired_height_rows.at_least(1) as f32) * line_spacing;
+        let desired_size = vec2(
+            galley.size.x.max(desired_width.min(available_width)),
+            galley.size.y.max(desired_height),
+        );
+        let rect = ui.allocate_space(desired_size);
+
+        let id = id.unwrap_or_else(|| {
+            if let Some(id_source) = id_source {
+                ui.make_persistent_id(id_source)
+            } else {
+                // Since we are only storing cursor, perfect persistence Id not super important
+                ui.make_position_id() // Must be after allocate_space!
+            }
+        });
+        let mut state = ui.memory().text_edit.get(&id).cloned().unwrap_or_default();
+
+        let sense = if enabled {
+            Sense::click_and_drag()
+        } else {
+            Sense::nothing()
+        };
+        let response = ui.interact(rect, id, sense);
+
+        if enabled {
+            ui.memory().interested_in_kb_focus(id);
+        }
+
+        if enabled {
+            if let Some(mouse_pos) = ui.input().mouse.pos {
+                // TODO: triple-click to select whole paragraph
+                // TODO: drag selected text to either move or clone (ctrl on windows, alt on mac)
+
+                let cursor_at_mouse = galley.cursor_from_pos(mouse_pos - response.rect.min);
+
+                if response.hovered {
+                    // preview:
+                    paint_cursor_end(ui, response.rect.min, &galley, &cursor_at_mouse);
+                }
+
+                if response.hovered && response.double_clicked {
+                    // Select word:
+                    let center = cursor_at_mouse;
+                    let ccursorp = select_word_at(text, center.ccursor);
+                    state.cursorp = Some(CursorPair {
+                        primary: galley.from_ccursor(ccursorp.primary),
+                        secondary: galley.from_ccursor(ccursorp.secondary),
+                    });
+                } else if response.hovered && ui.input().mouse.pressed {
+                    ui.memory().request_kb_focus(id);
+                    if ui.input().modifiers.shift {
+                        if let Some(cursorp) = &mut state.cursorp {
+                            cursorp.primary = cursor_at_mouse;
+                        } else {
+                            state.cursorp = Some(CursorPair::one(cursor_at_mouse));
+                        }
+                    } else {
+                        state.cursorp = Some(CursorPair::one(cursor_at_mouse));
+                    }
+                } else if ui.input().mouse.down && response.active {
+                    if let Some(cursorp) = &mut state.cursorp {
+                        cursorp.primary = cursor_at_mouse;
+                    }
+                }
+            }
+        }
+
+        if ui.input().mouse.pressed && !response.hovered {
+            // User clicked somewhere else
+            ui.memory().surrender_kb_focus(id);
+        }
+
+        if !enabled {
+            ui.memory().surrender_kb_focus(id);
+        }
+
+        if response.hovered && enabled {
+            ui.output().cursor_icon = CursorIcon::Text;
+        }
+
+        if ui.memory().has_kb_focus(id) && enabled {
+            let mut cursorp = state
+                .cursorp
+                .map(|cursorp| {
+                    // We only keep the PCursor (paragraph number, and character offset within that paragraph).
+                    // This is so what if we resize the `TextEdit` region, and text wrapping changes,
+                    // we keep the same byte character offset from the beginning of the text,
+                    // even though the number of rows changes
+                    // (each paragraph can be several rows, due to word wrapping).
+                    // The column (character offset) should be able to extend beyond the last word so that we can
+                    // go down and still end up on the same column when we return.
+                    CursorPair {
+                        primary: galley.from_pcursor(cursorp.primary.pcursor),
+                        secondary: galley.from_pcursor(cursorp.secondary.pcursor),
+                    }
+                })
+                .unwrap_or_else(|| CursorPair::one(galley.end()));
+
+            // We feed state to the undoer both before and after handling input
+            // so that the undoer creates automatic saves even when there are no events for a while.
+            state
+                .undoer
+                .feed_state(ui.input().time, &(cursorp.as_ccursorp(), text.clone()));
+
+            for event in &ui.input().events {
+                let did_mutate_text = match event {
+                    Event::Copy => {
+                        if cursorp.is_empty() {
+                            ui.ctx().output().copied_text = text.clone();
+                        } else {
+                            ui.ctx().output().copied_text = selected_str(text, &cursorp).to_owned();
+                        }
+                        None
+                    }
+                    Event::Cut => {
+                        if cursorp.is_empty() {
+                            ui.ctx().output().copied_text = std::mem::take(text);
+                            Some(CCursorPair::default())
+                        } else {
+                            ui.ctx().output().copied_text = selected_str(text, &cursorp).to_owned();
+                            Some(CCursorPair::one(delete_selected(text, &cursorp)))
+                        }
+                    }
+                    Event::Text(text_to_insert) => {
+                        // Newlines are handled by `Key::Enter`.
+                        if !text_to_insert.is_empty()
+                            && text_to_insert != "\n"
+                            && text_to_insert != "\r"
+                        {
+                            let mut ccursor = delete_selected(text, &cursorp);
+                            insert_text(&mut ccursor, text, text_to_insert);
+                            Some(CCursorPair::one(ccursor))
+                        } else {
+                            None
+                        }
+                    }
+                    Event::Key {
+                        key: Key::Enter,
+                        pressed: true,
                         modifiers,
                     } => {
 			if multiline && modifiers.command {
-			    println!("text {} {:?}", text, &cursorp);
+			    if let Some(cb) = eval_callback {
+				let mut cb_loc = cb.lock();
+				cb_loc(text);
+			    }
 			    break;
 			} else if multiline {
                             let mut ccursor = delete_selected(text, &cursorp);
